d9024784292e3227d9440c515b3841d4d2d2ebc1
Mike Hoskins <mrh@devopsdreams.io>
2021-08-21 12:18:55 -0400

    cleanup

--- common/nvim/.config/nvim/lua/commands.lua
+++ common/nvim/.config/nvim/lua/commands.lua
@@ -3,10 +3,11 @@ local cmd = vim.cmd
 cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
 cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
 cmd('colorscheme gruvbox-material')
+-- cmd('colorscheme everforest')
 cmd('filetype plugin indent on')
 -- transparent background
-cmd('highlight Normal ctermbg=NONE guibg=NONE')
-cmd('highlight NonText ctermbg=NONE guibg=NONE')
+-- cmd('highlight Normal ctermbg=NONE guibg=NONE')
+-- cmd('highlight NonText ctermbg=NONE guibg=NONE')
 -- cmd('syntax enable')
 cmd('syntax on')
 -- disable automatic comment insertion
