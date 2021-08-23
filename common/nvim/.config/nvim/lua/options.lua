69cfdb548efe1ee38625da365c8452d80b1b60d1
Mike Hoskins <mrh@devopsdreams.io>
2021-08-22 20:33:37 -0400

    cleanup

--- common/nvim/.config/nvim/lua/options.lua
+++ common/nvim/.config/nvim/lua/options.lua
@@ -1,7 +1,7 @@
-local o   = vim.opt     -- global options
-local wo  = vim.wo      -- window options
-local bo  = vim.bo      -- buffer options
-local g   = vim.g
+local o = vim.opt -- global options
+local wo = vim.wo -- window options
+local bo = vim.bo -- buffer options
+local g = vim.g
 
 -- example capability check
 -- local has = vim.fn.has
@@ -12,7 +12,6 @@ local g   = vim.g
 g.mapleader = ' ' -- must be set early
 g.elite_mode = 1 -- unbind arrows (reused to size splits)
 g.gruvbox_material_background = 'medium'
--- g.everforest_background = 'medium'
 
 o.background = 'dark'
 o.termguicolors = true
@@ -24,8 +23,10 @@ o.updatetime = 300 -- time (in ms) to write to swap file
 o.ttimeout = true -- adjust key sequence timeout
 o.ttimeoutlen = 100 -- ...
 o.clipboard = 'unnamed,unnamedplus' -- one clipboard to rule them all
-o.scrolloff = 10 -- 10 line scroll viewport
+o.scrolloff = 10 -- vertical scroll viewport
+o.sidescrolloff = 10 -- horizontal scroll viewport
 o.ignorecase = true -- case insensitive search
+o.joinspaces = false -- no double spaces on join
 o.smartcase = true -- try to be smart about case
 o.hlsearch = true -- highlight search
 o.incsearch = true -- no incremental search
@@ -46,6 +47,7 @@ o.wildmode = { 'longest', 'full' }
 o.wildignorecase = true
 o.splitright = true -- split to right
 o.splitbelow = true -- split to below
+o.shiftround = true -- round indent spacing to shiftwidth
 -- o.shortmess:append('c') -- status line e.g. CTRL+G
 
 -- no toxic mouse-culinity
@@ -60,12 +62,12 @@ bo.tabstop = 4 -- tab stop
 bo.softtabstop = 4 -- soft tab stop
 bo.shiftwidth = 4 -- auto indent shift width
 bo.autoindent = true -- auto indent for new line
--- -- bo.smartindent = true -- "smart" autoindenting
--- -- bo.cinwords = 'case,select,switch,if,elif,else,for,while,try,except,finally,def,func,class'
+bo.smartindent = true -- "smart" autoindenting
+-- bo.cinwords = 'case,select,switch,if,elif,else,for,while,try,except,finally,def,func,class'
 bo.modeline = true -- check for modelines
 
 wo.wrap = false -- disable line wrap
-wo.colorcolumn = "80" -- demarc long lines
+wo.colorcolumn = '80' -- demarc long lines
 wo.foldenable = false
 wo.number = true
 wo.relativenumber = true
