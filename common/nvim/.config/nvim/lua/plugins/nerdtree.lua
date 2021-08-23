5816a7bd5aab0c67ff5319aae1067b8a84875dad
Mike Hoskins <mrh@devopsdreams.io>
2021-08-22 20:57:51 -0400

    port to lua

--- common/nvim/.config/nvim/lua/plugins/nerdtree.lua
+++ common/nvim/.config/nvim/lua/plugins/nerdtree.lua
@@ -3,6 +3,11 @@ local g = vim.g
 
 cmd('autocmd BufNewFile,BufReadPost *.md set filetype=markdown')
 
-g.markdown_fenced_languages = { 'html', 'css', 'bash=sh', 'javascript', 'go' }
-g.markdown_syntax_conceal = 0
+g.NERDTreeShowHidden = 1
+g.NERDTreeMinimalUI = 1
+g.NERDTreeIgnore = { '^node_modules$' }
+g.NERDTreeStatusline = ''
+g.NERDTreeGitStatusWithFlags = 1
 
+-- Automaticaly close nvim if NERDTree is only thing left open
+cmd('autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif')
