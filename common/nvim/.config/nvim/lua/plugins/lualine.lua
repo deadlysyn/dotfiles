9e2f8f1c3c08d4be020ce5859b7e1ecdb63758fa
Mike Hoskins <mrh@devopsdreams.io>
2021-08-22 20:50:41 -0400

    cleanup

--- common/nvim/.config/nvim/lua/plugins/lualine.lua
+++ common/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,18 @@
 require('lualine').setup({
     options = {
         theme = 'gruvbox_material',
-        section_separators = '',
-        component_separators = '',
+        -- section_separators = '',
+        -- component_separators = '',
     },
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff' },
-        lualine_c = { 'filename' },
-        lualine_x = {
-            { 'diagnostics', sources = { 'nvim_lsp' } }
+        -- lualine_c = { 'filename' },
+        lualine_c = {
+            { 'diagnostics', sources = { 'nvim_lsp' } },
+            'filename'
         },
+        lualine_x = { },
         lualine_y = { 'encoding', 'fileformat', 'filetype' },
         lualine_z = { 'progress', 'location' }
     },
