63b38b3276a2f47e25985ccdf3676c365606d495
Mike Hoskins <mrh@devopsdreams.io>
2021-08-22 22:37:03 -0400

    cleanup

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -1,20 +1,24 @@
 -- https://github.com/crispgm/dotfiles/blob/main/nvim
 -- https://oroques.dev/notes/neovim-init
+-- TODO: hot reload
+-- https://neovim.discourse.group/t/reload-init-lua-and-all-require-d-scripts/971
+-- https://github.com/ThePrimeagen/.dotfiles/blob/master/nvim/.config/nvim/lua/theprimeagen/init.lua
 local try_require = require('common').try_require
 
 try_require('commands')
 try_require('options')
-try_require('pack')
+try_require('packages')
 try_require('mappings')
--- plugins
 try_require('plugins.compe')
+try_require('plugins.devicons')
 try_require('plugins.emmet')
-try_require('plugins.gitgutter')
-try_require('plugins.lualine')
+try_require('plugins.gitsigns')
 try_require('plugins.lspconfig')
+try_require('plugins.lualine')
+try_require('plugins.markdown')
+try_require('plugins.nerdtree')
 try_require('plugins.prettier')
 try_require('plugins.rust')
 try_require('plugins.telescope')
 try_require('plugins.treesitter')
 try_require('plugins.vsnip')
-
