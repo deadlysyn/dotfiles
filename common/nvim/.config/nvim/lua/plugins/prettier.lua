ea28e163e26be9aba7a4837c3f991154415a704c
Mike Hoskins <mrh@devopsdreams.io>
2021-08-22 20:40:29 -0400

    cleanup

--- common/nvim/.config/nvim/lua/plugins/prettier.lua
+++ common/nvim/.config/nvim/lua/plugins/prettier.lua
@@ -1,4 +1,13 @@
 local vim = vim
-vim.g['prettier#autoformat'] = 1
-vim.g['prettier#autoformat_require_pragma'] = 0
-vim.g['prettier#exec_cmd_path'] = vim.fn.exepath('prettier')
+local cmd = vim.cmd
+local g   = vim.g
+
+g['prettier#autoformat'] = 1
+g['prettier#autoformat_require_pragma'] = 0
+g['prettier#exec_cmd_async'] = 1
+g['prettier#quickfix_enabled'] = 0 -- doesn't play well with async
+g['prettier#exec_cmd_path'] = vim.fn.exepath('prettier')
+
+cmd(
+    'autocmd TextChanged,InsertLeave *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.svelte,*.yaml,*.yml,*.html PrettierAsync'
+)
