e9b2bafe1e8d5e3b571325842530bb11fb759612
Mike Hoskins <mrh@devopsdreams.io>
2021-08-22 20:21:04 -0400

    enable diagnostics

--- common/nvim/.config/nvim/lua/plugins/lualine.lua
+++ common/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,14 +3,16 @@ require('lualine').setup({
         theme = 'gruvbox_material',
         section_separators = '',
         component_separators = '',
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch' },
-            lualine_c = { 'diagnostics', 'filename' },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { 'filename' },
+        lualine_x = {
+            { 'diagnostics', sources = { 'nvim_lsp' } }
         },
-        extensions = { 'fugitive', 'quickfix' }
-    }
+        lualine_y = { 'encoding', 'fileformat', 'filetype' },
+        lualine_z = { 'progress', 'location' }
+    },
+    extensions = { 'quickfix' }
 })
