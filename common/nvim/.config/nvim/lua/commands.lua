af53add1b52e41b89275ba8e359f38375bd7d8ba
Mike Hoskins <mrh@devopsdreams.io>
2021-08-26 23:17:38 -0400

    2-space tab defaults; use rg

--- common/nvim/.config/nvim/lua/commands.lua
+++ common/nvim/.config/nvim/lua/commands.lua
@@ -1,21 +1,18 @@
 local cmd = vim.cmd
 
-cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
-cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
+-- cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
+-- cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
 cmd('colorscheme gruvbox-material')
--- cmd('colorscheme everforest')
 cmd('filetype plugin indent on')
--- transparent background
--- cmd('highlight Normal ctermbg=NONE guibg=NONE')
--- cmd('highlight NonText ctermbg=NONE guibg=NONE')
 cmd('syntax on')
+cmd('set grepprg=rg')
+cmd('set grepformat=%f:%l:%c:%m')
 -- disable automatic comment insertion
 cmd(
-    'autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o'
+  'autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o'
 )
 -- delete comment character when joining lines
 cmd('autocmd FileType * setlocal formatoptions+=j')
--- return to last edit position
 -- turn on spell check for select file types
 cmd('autocmd BufRead,BufNewFile *.md setlocal spell')
 cmd('autocmd BufRead,BufNewFile *.rst setlocal spell')
@@ -23,9 +20,9 @@ cmd('autocmd BufRead,BufNewFile *.html setlocal spell')
 cmd('autocmd BufRead,BufNewFile *.txt setlocal spell')
 cmd('autocmd FileType gitcommit setlocal spell')
 cmd('autocmd Filetype gitcommit setlocal spell textwidth=70')
--- use two space tabs for yaml
+-- use four space tabs for python
 cmd(
-    'autocmd FileType yaml setlocal tabstop=2 expandtab shiftwidth=2 softtabstop=2'
+  'autocmd FileType yaml setlocal tabstop=4 expandtab shiftwidth=4 softtabstop=4'
 )
 -- auto format
 cmd('autocmd BufWritePre *.html lua vim.lsp.buf.formatting_sync(nil, 100)')
@@ -36,3 +33,4 @@ cmd('autocmd BufWritePre *.json lua vim.lsp.buf.formatting_sync(nil, 100)')
 cmd('autocmd BufWritePre *.sh lua vim.lsp.buf.formatting_sync(nil, 100)')
 cmd('autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 100)')
 cmd('autocmd BufWritePre *.yml lua vim.lsp.buf.formatting_sync(nil, 100)')
+cmd('autocmd BufWritePre *.coffee lua vim.lsp.buf.formatting_sync(nil, 100)')
