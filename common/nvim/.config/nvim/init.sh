72def3b3f96e181ce567307f879b9427b31ea354
Mike Hoskins <mrh@devopsdreams.io>
2021-08-26 23:14:17 -0400

    cleanup

--- common/nvim/.config/nvim/init.sh
+++ common/nvim/.config/nvim/init.sh
@@ -4,31 +4,37 @@
 git clone https://github.com/wbthomason/packer.nvim \
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-yarn global add bash-language-server
-yarn global add vscode-css-languageserver-bin
 go get golang.org/x/tools/gopls@latest
-yarn global add vscode-html-languageserver-bin
-yarn global add vscode-json-languageserver
-yarn global add pyright
-brew install rust-analyzer
-yarn global add sql-language-server
-yarn global add typescript typescript-language-server
-yarn global add vim-language-server
-yarn global add vls
+
+# npm i -g ansible-language-server
+npm i -g bash-language-server
+npm i -g coffeelint
+npm i -g diagnostic-languageserver
+npm i -g dockerfile-language-server-nodejs
+# npm i -g pyright
+npm i -g sql-language-server
+npm i -g typescript typescript-language-server
+npm i -g vscode-langservers-extracted
+
 yarn global add yaml-language-server
 
+yay -S rust-analyzer
+yay -S shellcheck
+yay -S shfmt
+yay -S terraform-ls-bin
+
 # Lua LSP
-cd ~/bin
+cd ~/bin || exit
 wget https://hub.fastgit.org/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
 unzip ninja*zip
 rm ninja*zip
-cd ~/src
+cd ~/src || exit
 git clone https://github.com/sumneko/lua-language-server
-cd lua-language-server
+cd lua-language-server || exit
 git submodule update --init --recursive
-cd 3rd/luamake
+cd 3rd/luamake || exit
 compile/install.sh
 cd ../..
 ./3rd/luamake/luamake rebuild
 
-echo "Run `nvim +PackerInstall` to finish setup"
+echo "Run nvim +PackerInstall to finish setup"
