9e767ac8f6acfe2d53dfde6e17cf8ea65a04b0c6
Mike Hoskins <mrh@devopsdreams.io>
2021-08-27 21:56:45 -0400

    s/compe/cmp

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -1,15 +1,19 @@
+-- inspiration:
 -- https://github.com/crispgm/dotfiles/blob/main/nvim
 -- https://oroques.dev/notes/neovim-init
+
 -- TODO: hot reload
 -- https://neovim.discourse.group/t/reload-init-lua-and-all-require-d-scripts/971
 -- https://github.com/ThePrimeagen/.dotfiles/blob/master/nvim/.config/nvim/lua/theprimeagen/init.lua
+
 local try_require = require('common').try_require
 
 try_require('commands')
 try_require('options')
 try_require('packages')
 try_require('mappings')
-try_require('plugins.compe')
+-- try_require('plugins.compe')
+try_require('plugins.cmp')
 try_require('plugins.devicons')
 try_require('plugins.emmet')
 try_require('plugins.gitsigns')
@@ -19,6 +23,7 @@ try_require('plugins.markdown')
 try_require('plugins.nerdtree')
 try_require('plugins.prettier')
 try_require('plugins.rust')
+-- try_require('plugins.tabnine')
 try_require('plugins.telescope')
 try_require('plugins.treesitter')
 try_require('plugins.vsnip')
