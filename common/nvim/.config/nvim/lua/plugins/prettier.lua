c2427303fadd7f16abc181da063bc5e81dc633de
Mike Hoskins <mrh@devopsdreams.io>
2021-08-27 22:01:28 -0400

    less aggressive/annoying formatting

--- common/nvim/.config/nvim/lua/plugins/prettier.lua
+++ common/nvim/.config/nvim/lua/plugins/prettier.lua
@@ -1,14 +1,15 @@
 local vim = vim
-local cmd = vim.cmd
 local g   = vim.g
+-- local cmd = vim.cmd
 
-g['prettier#autoformat'] = 1
-g['prettier#autoformat_require_pragma'] = 0
+-- g['prettier#autoformat'] = 1
+-- g['prettier#autoformat_require_pragma'] = 0
 g['prettier#autoformat_config_files'] = { '.prettierrc', '.prettierrc.yml', '.prettierrc.yaml', '.prettierrc.json' }
 g['prettier#exec_cmd_async'] = 0
-g['prettier#quickfix_enabled'] = 1 -- doesn't play well with async
+g['prettier#quickfix_enabled'] = 0 -- doesn't play well with async
 g['prettier#exec_cmd_path'] = vim.fn.exepath('prettier')
 
-cmd(
-    'autocmd TextChanged,InsertLeave *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.svelte,*.yaml,*.yml,*.html PrettierAsync'
-)
+-- w/o this must trigger with <leader>p
+-- cmd(
+--     'autocmd TextChanged,InsertLeave *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.svelte,*.yaml,*.yml,*.html PrettierAsync'
+-- )
