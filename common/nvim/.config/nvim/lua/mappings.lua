9f0a89f7494c466373525f566a41f70e3a2fbf34
Mike Hoskins <mrh@devopsdreams.io>
2021-08-30 23:52:19 -0400

    cleanup

--- common/nvim/.config/nvim/lua/mappings.lua
+++ common/nvim/.config/nvim/lua/mappings.lua
@@ -75,42 +75,19 @@ set_keymap("n", "<leader>]", ":bnext<cr>")
 -- set_keymap('n', '<leader>cd', ':cd %:p:h<cr>:pwd<cr>')
 
 -- lsp
-set_keymap("n", "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<cr>")
-set_keymap("n", "<leader>lD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-set_keymap("n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-set_keymap("n", "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<cr>")
--- set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-set_keymap("n", "U", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-set_keymap("n", "<leader>lr", "<cmd>Telescope lsp_references<cr>")
-set_keymap("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>")
-set_keymap("n", "<leader>lS", "<cmd>Telescope lsp_workspace_symbols<cr>")
-set_keymap("n", "<leader>lR", "<cmd>lua vim.lsp.buf.rename()<cr>")
-set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>")
+set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
+set_keymap("n", "gr", "<cmd>Telescope lsp_references<cr>")
+set_keymap("n", "gs", "<cmd>Telescope lsp_document_symbols<cr>")
+-- set_keymap("n", "<leader>lS", "<cmd>Telescope lsp_workspace_symbols<cr>")
+set_keymap("n", "<leader>R", "<cmd>lua vim.lsp.buf.rename()<cr>")
+set_keymap("n", "<leader>F", "<cmd>lua vim.lsp.buf.formatting()<cr>")
 set_keymap("n", "<c-n>", "<cmd>lua vim.lsp.diagnostic.goto_next()<cr>")
 set_keymap("n", "<c-p>", "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>")
-
--- lspsaga
--- find cursor word definition and reference
-set_keymap("n", "gh", "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>")
--- show hover doc
-set_keymap("n", "K", "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>")
--- scroll down hover doc or scroll in definition preview
-set_keymap("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
--- scroll up hover doc
-set_keymap("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
--- show signature help
-set_keymap("n", "gs", "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>")
--- rename
-set_keymap("n", "gr", "<cmd>lua require('lspsaga.rename').rename()<CR>")
--- preview definition
-set_keymap("n", "gd", "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>")
--- show diagnostics
-set_keymap("n", "<leader>cd", "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>")
--- only show diagnostic if cursor is over the area
-set_keymap("n", "<leader>cc", "<cmd>lua require'lspsaga.diagnostic'.show_cursor_diagnostics()<CR>")
--- jump diagnostic
-set_keymap("n", "[e", "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>")
-set_keymap("n", "]e", "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>")
 
 -- telescope
 set_keymap("n", "<leader>ff", "<cmd>Telescope git_files<cr>")
@@ -121,9 +98,6 @@ set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 set_keymap("n", "<leader>fl", "<cmd>Telescope lsp_document_symbols<cr>")
 set_keymap("n", "<leader>fk", "<cmd>Telescope keymaps<cr>")
 set_keymap("n", "<leader>fm", "<cmd>Telescope heading<cr>")
-
--- nerdtree
-set_keymap("n", "<leader>b", "<cmd>NERDTreeToggle<cr>")
 
 -- disable search highlight
 set_keymap("n", "<leader><cr>", ":noh<cr>")
