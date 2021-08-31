2a75423edcb5f018a0cd31b830d32ef376f216fb
Mike Hoskins <mrh@devopsdreams.io>
2021-08-30 22:52:50 -0400

    cleanup

--- common/nvim/.config/nvim/lua/plugins/lualine.lua
+++ common/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,6 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff" },
-		-- lualine_c = { 'filename' },
 		lualine_c = {
 			{ "diagnostics", sources = { "nvim_lsp" } },
 			"filename",
