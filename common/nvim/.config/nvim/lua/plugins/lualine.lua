a40a6a7f3696b92018ec78507d44df74d39e034c
Mike Hoskins <mrh@devopsdreams.io>
2021-09-24 15:42:19 -0400

    adjust diagnostic formatting

--- common/nvim/.config/nvim/lua/plugins/lualine.lua
+++ common/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,28 @@
 require("lualine").setup({
-	options = {
-		theme = "gruvbox_material",
-		section_separators = '',
-		component_separators = '',
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff" },
-		lualine_c = {
-			{ "diagnostics", sources = { "nvim_lsp" } },
-			"filename",
-		},
-		lualine_x = {},
-		lualine_y = { "encoding", "fileformat", "filetype" },
-		lualine_z = { "progress", "location" },
-	},
-	extensions = { "quickfix" },
+  options = {
+    theme = "gruvbox_material",
+    section_separators = '',
+    component_separators = '',
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "filename" },
+    lualine_c = {
+      {
+        "diagnostics",
+        sources = { "nvim_lsp" },
+        sections = {"error", "warn", "info", "hint"},
+        -- all colors are in format #rrggbb
+        color_error = "#aa0000",
+        color_warn = "#aaaa00",
+        color_info = "#aaaaaa",
+        color_hint = "#aaaaaa",
+        symbols = {error = "E", warn = "W", info = "I", hint = "H"}
+      }
+    },
+    lualine_x = {},
+    lualine_y = { "encoding", "fileformat", "filetype" },
+    lualine_z = { "progress", "location" },
+  },
+  extensions = { "quickfix" },
 })
