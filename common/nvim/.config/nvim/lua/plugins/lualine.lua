d7672d57a68eb42fce756299793c2a8da7ef6c18
Mike Hoskins <mrh@devopsdreams.io>
2021-09-24 20:51:47 -0400

    match colors

--- common/nvim/.config/nvim/lua/plugins/lualine.lua
+++ common/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,17 +6,29 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "filename" },
+    lualine_b = {
+      "branch",
+      {
+        "diff",
+        colored = true, -- displays diff status in color if set to true
+        -- all colors are in format #rrggbb
+        color_added = "#a9b665", -- changes diff's added foreground color
+        color_modified = "#7daea3", -- changes diff's modified foreground color
+        color_removed = "#ea6962", -- changes diff's removed foreground color
+        symbols = {added = '+', modified = '~', removed = '-'}
+      },
+      "filename"
+    },
     lualine_c = {
       {
         "diagnostics",
         sources = { "nvim_lsp" },
         sections = {"error", "warn", "info", "hint"},
         -- all colors are in format #rrggbb
-        color_error = "#aa0000",
-        color_warn = "#aaaa00",
-        color_info = "#aaaaaa",
-        color_hint = "#aaaaaa",
+        color_error = "#ea6962",
+        color_warn = "#d8a657",
+        color_info = "#7daea3",
+        color_hint = "#7daea3",
         symbols = {error = "E", warn = "W", info = "I", hint = "H"}
       }
     },
