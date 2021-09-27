85b5dc6e7bcb97a3b17d882a7c6e57aa63b135a7
Mike Hoskins <mrh@devopsdreams.io>
2021-09-27 12:13:49 -0400

    simplify

--- common/nvim/.config/nvim/lua/options.lua
+++ common/nvim/.config/nvim/lua/options.lua
@@ -1,7 +1,4 @@
-local o = vim.opt -- global options
-local wo = vim.wo -- window options
-local bo = vim.bo -- buffer options
-local g = vim.g
+-- https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua
 
 -- example capability check
 -- local has = vim.fn.has
@@ -9,72 +6,90 @@ local g = vim.g
 --     opt.t_Co = 256
 -- end
 
+-- call a function
+-- vim.call('fzf#vim#files', '~/projects', false)
+
+-- vim.env gets or sets env vars
+-- vim.env.FZF_DEFAULT_OPTS = '--layout=reverse'
+
+-- some vimscript var names are not valid in lua.
+-- set this:
+--  let g:zoom#statustext = 'Z'
+-- like this:
+--  g['zoom#statustext'] = 'Z'
+local g = vim.g -- global variables
 g.t_Co = 256 -- color term support
 g.mapleader = " " -- must be set early
 g.elite_mode = 1 -- unbind arrows (reused to size splits)
 g.gruvbox_material_background = "medium"
 
-o.background = "dark"
-o.termguicolors = true
-o.encoding = "utf-8" -- set internal encoding
-o.fileencodings = { "utf-8" } -- available encodings
-o.hidden = true -- hide abandoned buffers
-o.backup = false -- no .bak
-o.updatetime = 300 -- time (in ms) to write to swap file
-o.ttimeout = true -- adjust key sequence timeout
-o.ttimeoutlen = 100 -- ...
-o.clipboard = "unnamed,unnamedplus" -- one clipboard to rule them all
-o.scrolloff = 5 -- vertical scroll viewport
-o.sidescrolloff = 5 -- horizontal scroll viewport
-o.ignorecase = true -- case insensitive search
-o.joinspaces = false -- no double spaces on join
-o.smartcase = true -- try to be smart about case
-o.hlsearch = true -- highlight search
-o.incsearch = true -- no incremental search
-o.inccommand = "nosplit" -- live substitute preview
-o.whichwrap = "b,h,l,s,<,>,[,]" -- cursor scrolls past/over line end
-o.backspace = { "indent", "eol", "start" } -- what backspace can traverse
-o.showmatch = true -- show bracket match
-o.errorbells = false -- no annoying sounds on errors
-o.visualbell = false -- no annoying flash on errors
-o.warn = true -- warn on unsved changes
-o.cmdheight = 2 -- height of :command line
-o.ruler = true -- show line/column numbers
-o.switchbuf = "useopen,usetab,newtab" -- what to consider when switching bufs
-o.showtabline = 2 -- always show tabline
-o.completeopt = "menuone,noselect"
-o.wildmenu = true -- wildmenu, auto complete for commands
-o.wildmode = { "longest", "full" }
-o.wildignore:append("**/.git/*", "**/node_modules/*")
-o.wildignorecase = true
-o.splitright = true -- split to right
-o.splitbelow = true -- split to below
-o.shiftround = true -- round indent spacing to shiftwidth
-o.shortmess = "atI"
+-- use these to read values of specific type...
+-- local o = vim.opt -- global options
+-- local wo = vim.wo -- window options
+-- local bo = vim.bo -- buffer options
+-- use this to set values of any type...
+local set = vim.opt
+-- global options
+set.background = "dark"
+set.termguicolors = true
+set.encoding = "utf-8" -- set internal encoding
+set.fileencodings = "utf-8" -- available encodings
+set.hidden = true -- hide abandoned buffers
+set.backup = false -- no .bak
+set.updatetime = 300 -- time (in ms) to write to swap file
+set.ttimeout = true -- adjust key sequence timeout
+set.ttimeoutlen = 100 -- ...
+set.clipboard = "unnamed,unnamedplus" -- one clipboard to rule them all
+set.scrolloff = 5 -- vertical scroll viewport
+set.sidescrolloff = 5 -- horizontal scroll viewport
+set.ignorecase = true -- case insensitive search
+set.joinspaces = false -- no double spaces on join
+set.smartcase = true -- try to be smart about case
+set.hlsearch = true -- highlight search
+set.incsearch = true -- no incremental search
+set.inccommand = "nosplit" -- live substitute preview
+set.whichwrap = "b,h,l,s,<,>,[,]" -- cursor scrolls past/over line end
+set.backspace = "indent,eol,start" -- what backspace can traverse
+set.showmatch = true -- show bracket match
+set.errorbells = false -- no annoying sounds on errors
+set.visualbell = false -- no annoying flash on errors
+set.warn = true -- warn on unsved changes
+set.cmdheight = 2 -- height of :command line
+set.ruler = true -- show line/column numbers
+set.switchbuf = "useopen,usetab,newtab" -- what to consider when switching bufs
+set.showtabline = 2 -- always show tabline
+set.completeopt = "menuone,noselect"
+set.wildmenu = true -- wildmenu, auto complete for commands
+set.wildmode = "longest,full"
+set.wildignore = "**/cache/*,**/tmp/*,**/.git/*,**/node_modules/*"
+set.wildignorecase = true
+set.splitright = true -- split to right
+set.splitbelow = true -- split to below
+set.shiftround = true -- round indent spacing to shiftwidth
+set.shortmess = "atI"
 -- o.shortmess:append('c') -- status line e.g. CTRL+G
+-- o.shortmess:prepend('c') -- status line e.g. CTRL+G
 
--- no toxic mouse-culinity
--- o.mouse = 'nv' -- enable mouse under normal and visual mode
--- cmd('set mousehide') -- hide mouse when characters are typed
+-- buffer options
+set.fileencoding = "utf-8" -- buffer encoding
+set.swapfile = false -- no .swap
+set.undofile = true -- use undo file
+set.expandtab = true -- expand tabs to spaces
+set.tabstop = 2 -- tab stop
+set.softtabstop = 2 -- soft tab stop
+set.shiftwidth = 2 -- auto indent shift width
+set.autoindent = true -- auto indent for new line
+set.smartindent = true -- "smart" autoindenting
+set.modeline = false -- check for modelines; security risk!
 
-bo.fileencoding = "utf-8" -- buffer encoding
-bo.swapfile = false -- no .swap
-bo.undofile = true -- use undo file
-bo.expandtab = true -- expand tabs to spaces
-bo.tabstop = 2 -- tab stop
-bo.softtabstop = 2 -- soft tab stop
-bo.shiftwidth = 2 -- auto indent shift width
-bo.autoindent = true -- auto indent for new line
-bo.smartindent = true -- "smart" autoindenting
-bo.modeline = false -- check for modelines; security risk!
-
-wo.wrap = false -- disable line wrap
-wo.colorcolumn = "80" -- demarc long lines
-wo.foldenable = false
-wo.number = true
-wo.relativenumber = true
-wo.cursorline = true -- highlight current line
-wo.cursorcolumn = true -- highlight current column
-wo.list = true -- show tabs with listchars
-wo.listchars = "tab:>-,trail:.,extends:#,nbsp:."
-wo.signcolumn = "yes" -- show sign column
+-- window options
+set.wrap = false -- disable line wrap
+set.colorcolumn = "80" -- demarc long lines
+set.foldenable = false
+set.number = true
+set.relativenumber = true
+set.cursorline = true -- highlight current line
+set.cursorcolumn = true -- highlight current column
+set.list = true -- show tabs with listchars
+set.listchars = "tab:>-,trail:.,extends:#,nbsp:."
+set.signcolumn = "yes" -- show sign column
