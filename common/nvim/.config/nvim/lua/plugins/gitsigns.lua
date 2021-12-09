5af1114c007c6f197ff0d9779743c46f20653c61
Mike Hoskins <me@deadlysyn.com>
2021-12-09 13:14:06 -0500

    update sign chars

--- common/nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ common/nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,35 +1,13 @@
 require("gitsigns").setup({
 	signs = {
-		add = {
-			hl = "GitSignsAdd",
-			text = "▍",
-			numhl = "GitSignsAddNr",
-			linehl = "GitSignsAddLn",
-		},
-		change = {
-			hl = "GitSignsChange",
-			text = "▍",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-		delete = {
-			hl = "GitSignsDelete",
-			text = "_",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		topdelete = {
-			hl = "GitSignsDelete",
-			text = "‾",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		changedelete = {
-			hl = "GitSignsChange",
-			text = "~",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
+		add = { hl = "GitSignsAdd", text = "▍" },
+		change = { hl = "GitSignsChange", text = "▍" },
+		delete = { hl = "GitSignsDelete", text = "▍" },
+		topdelete = { hl = "GitSignsDelete", text = "▍" },
+		changedelete = { hl = "GitSignsChange", text = "▍" },
+		-- delete = { hl = "GitSignsDelete", text = "_" },
+		-- topdelete = { hl = "GitSignsDelete", text = "‾" },
+		-- changedelete = { hl = "GitSignsChange", text = "~" },
 	},
 	numhl = false,
 	linehl = false,
@@ -59,14 +37,14 @@ require("gitsigns").setup({
 		["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
 		["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
 	},
-	watch_gitdir = {
-		interval = 1000,
-		follow_files = true,
-	},
-	current_line_blame = false,
-	sign_priority = 6,
-	update_debounce = 100,
-	status_formatter = nil, -- Use default
-	word_diff = false,
+	-- watch_gitdir = {
+	-- 	interval = 1000,
+	-- 	follow_files = true,
+	-- },
+	current_line_blame = true,
+	-- sign_priority = 6,
+	-- update_debounce = 100,
+	-- status_formatter = nil, -- Use default
+	-- word_diff = false,
 	-- use_internal_diff = true, -- If luajit is present
 })
