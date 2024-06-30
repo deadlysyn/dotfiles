b5234d72948464ce7648da9e7290d1df7ba5d038
Mike Hoskins <me@deadlysyn.com>
2024-06-29 21:16:10 -0400

    options cleanup; better keymaps

--- common/lvim/.config/lvim/config.lua
+++ common/lvim/.config/lvim/config.lua
@@ -26,31 +26,31 @@ require('Comment').setup()
 -- require('lspconfig').eslint.setup({})
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#graphql
 -- require 'lspconfig'.graphql.setup {}
-local linters = require('lvim.lsp.null-ls.linters')
-linters.setup({
-  {
-    command = 'eslint',
-    -- args = { "--no-warn-ignored" },
-    filetypes = {
-      'javascript',
-      'javascriptreact',
-      'typescript',
-      'typescriptreact',
-    },
-  },
-})
-local formatters = require('lvim.lsp.null-ls.formatters')
-formatters.setup({
-  {
-    command = 'prettier',
-    filetypes = {
-      'javascript',
-      'javascriptreact',
-      'typescript',
-      'typescriptreact',
-    },
-  },
-})
+-- local linters = require('lvim.lsp.null-ls.linters')
+-- linters.setup({
+--   {
+--     command = 'eslint',
+--     -- args = { "--no-warn-ignored" },
+--     filetypes = {
+--       'javascript',
+--       'javascriptreact',
+--       'typescript',
+--       'typescriptreact',
+--     },
+--   },
+-- })
+-- local formatters = require('lvim.lsp.null-ls.formatters')
+-- formatters.setup({
+--   {
+--     command = 'prettier',
+--     filetypes = {
+--       'javascript',
+--       'javascriptreact',
+--       'typescript',
+--       'typescriptreact',
+--     },
+--   },
+-- })
 
 -- import extra configs
 local try_require = require('util').try_require
---
+++ common/lvim/.config/lvim/lua/keymaps.lua
@@ -0,0 +1,33 @@
+-- toggle neotree
+lvim.builtin.which_key.mappings['e'] = {
+  '<cmd>Neotree toggle<CR>',
+  '', -- this is required!
+}
+
+-- <tab> and <shift-tab> navigation
+lvim.keys.normal_mode['<tab>'] = ':bn<CR>'
+lvim.keys.normal_mode['<s-tab>'] = ':bp<CR>'
+
+-- move selected lines up/down
+lvim.keys.visual_mode['J'] = ":m '>+1<CR>gv=gv"
+lvim.keys.visual_mode['K'] = ":m '<-2<CR>gv=gv"
+
+-- keep cursor at same place when joining
+lvim.keys.normal_mode['J'] = 'mzJ`z'
+-- keep cursor in middle of screen when jumping
+lvim.keys.normal_mode['<c-d>'] = '<c-d>zz'
+lvim.keys.normal_mode['<c-u>'] = '<c-u>zz'
+-- keep search terms in middle
+lvim.keys.normal_mode['n'] = 'nzzzv'
+lvim.keys.normal_mode['N'] = 'Nzzzv'
+
+-- paste over selection without loosing buffer content
+lvim.keys.visual_mode['<leader>p'] = '\"_dP'
+
+-- <leader>y yanks to system clipboard
+lvim.keys.normal_mode['<leader>y'] = '\"+y'
+lvim.keys.normal_mode['<leader>Y'] = '\"+Y'
+lvim.keys.visual_mode['<leader>y'] = '\"+y'
+
+-- does anyone use this?
+lvim.keys.normal_mode['Q'] = '<nop>'
