f38d0a38afd5102f64f5983f84dbc305f7bb6979
Mike Hoskins <me@deadlysyn.com>
2024-07-05 14:06:02 -0400

    re-enable typescript things

--- common/lvim/.config/lvim/config.lua
+++ common/lvim/.config/lvim/config.lua
@@ -23,34 +23,34 @@ local ft = require('Comment.ft')
 ft.hcl = { '#%s', '/*%s*/' }
 require('Comment').setup()
 
--- require('lspconfig').eslint.setup({})
+require('lspconfig').eslint.setup({})
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#graphql
--- require 'lspconfig'.graphql.setup {}
--- local linters = require('lvim.lsp.null-ls.linters')
--- linters.setup({
---   {
---     command = 'eslint',
---     -- args = { "--no-warn-ignored" },
---     filetypes = {
---       'javascript',
---       'javascriptreact',
---       'typescript',
---       'typescriptreact',
---     },
---   },
--- })
--- local formatters = require('lvim.lsp.null-ls.formatters')
--- formatters.setup({
---   {
---     command = 'prettier',
---     filetypes = {
---       'javascript',
---       'javascriptreact',
---       'typescript',
---       'typescriptreact',
---     },
---   },
--- })
+require 'lspconfig'.graphql.setup {}
+local linters = require('lvim.lsp.null-ls.linters')
+linters.setup({
+  {
+    command = 'eslint',
+    -- args = { "--no-warn-ignored" },
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+    },
+  },
+})
+local formatters = require('lvim.lsp.null-ls.formatters')
+formatters.setup({
+  {
+    command = 'prettier',
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+    },
+  },
+})
 
 -- import extra configs
 local try_require = require('util').try_require
