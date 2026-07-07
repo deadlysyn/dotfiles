13b4297e28561ed925024e9fd7269a24c9109bf2
Mike Hoskins <mike@assured.claims>
2026-07-07 01:20:53 -0400

    vim overhaul

--- common/nvim/.config/nvim/lua/config/autocommands.lua
+++ common/nvim/.config/nvim/lua/config/autocommands.lua
@@ -25,18 +25,13 @@ autocmd({ 'FileType' }, {
     end,
 })
 
--- turn on spell check for select file types
-autocmd({ 'BufRead', 'BufNewFile' }, {
-    pattern = { '*.md', '*.rst', '*.html', '*.txt' },
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup('wrap_spell'),
+    pattern = { 'text', 'plaintex', 'typst', 'gitcommit', 'markdown' },
     callback = function()
+        vim.opt_local.wrap = true
         vim.opt_local.spell = true
-    end,
-})
-autocmd({ 'FileType' }, {
-    pattern = { 'gitcommit' },
-    callback = function()
-        vim.opt_local.spell = true
-        vim.opt_local.textwidth = 70
     end,
 })
 
@@ -69,6 +64,54 @@ autocmd({ 'FileType' }, {
         vim.opt_local.shiftwidth = 4
         vim.opt_local.softtabstop = 4
         vim.opt_local.tabstop = 4
+    end,
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+    group = augroup('json_conceal'),
+    pattern = { 'json', 'jsonc', 'json5' },
+    callback = function()
+        vim.opt_local.conceallevel = 0
+    end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    group = augroup('auto_create_dir'),
+    callback = function(event)
+        if event.match:match('^%w%w+:[\\/][\\/]') then
+            return
+        end
+        local file = vim.uv.fs_realpath(event.match) or event.match
+        vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+    end,
+})
+
+-- Set filetype for .env and .env.* files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    group = augroup('env_filetype'),
+    pattern = { '*.env', '.env.*' },
+    callback = function()
+        vim.opt_local.filetype = 'sh'
+    end,
+})
+
+-- Set filetype for .toml files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    group = augroup('toml_filetype'),
+    pattern = { '*.tomg-config*' },
+    callback = function()
+        vim.opt_local.filetype = 'toml'
+    end,
+})
+
+-- Set filetype for .ejs files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    group = augroup('ejs_filetype'),
+    pattern = { '*.ejs', '*.ejs.t' },
+    callback = function()
+        vim.opt_local.filetype = 'embedded_template'
     end,
 })
 
